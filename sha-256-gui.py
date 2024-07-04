6ed97f753ba7e6d2a086f48412bee6733bcc152c
Yoda-Flash <kaitlyn.zee@gmail.com>
2024-07-04 08:36:45 +0800

    Added the necessary componenets but still some work

--- sha-256-gui.py
+++ sha-256-gui.py
@@ -2,15 +2,27 @@ import tkinter as tk
 from tkinter import Entry, Label
 from tkinter import ttk
 
-tk = tk.Tk()
+root = tk.Tk()
 
-tk.title('SHA-256 Hasher')
-combobox = ttk.Combobox(tk, values=['SHA-256', 'AES-GCM'])
+def on_select(event):
+    mode = combobox.get()
+    print(mode)
+    if mode == 'SHA-256':
+        Label(root, text='Message to hash:').pack()
+        message = Entry(root)
+        message.pack()
+    elif mode == 'AES-GCM':
+        Label(root, text='Message to transfer:').pack()
+        message = Entry(root)
+        message.pack()
+        Label(root, text='Secret key:').pack()
+        secret = Entry(root)
+        secret.pack()
+
+root.title('SHA-256 Hasher')
+combobox = ttk.Combobox(root, values=['SHA-256', 'AES-GCM'])
 combobox.pack(pady=20)
 combobox.set('SHA-256')
-mode = combobox.get()
-# if mode == 'SHA-256':
-Label(tk, text='Message to hash:')
-message = Entry(tk)
+combobox.bind('<<ComboboxSelected>>', on_select)
 
-tk.mainloop()
+root.mainloop()
